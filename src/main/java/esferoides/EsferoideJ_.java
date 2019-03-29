8cf75b7fb92b6ed7cf20f8b06d7def921aefbc36
Jonathan <joheras@gmail.com>
2019-03-29 10:25:12 +0100

    Changed code for black holes

--- src/main/java/esferoides/EsferoideJ_.java
+++ src/main/java/esferoides/EsferoideJ_.java
@@ -136,9 +136,9 @@ public class EsferoideJ_ implements Command {
 		int count = countBelowThreshold(imp2, 1800);
 		if (count > 10000) {
 
-			IJ.setThreshold(imp2, 0, 2600);
+			IJ.setThreshold(imp2, 0, 1500);
 			IJ.run(imp2, "Convert to Mask", "");
-			IJ.run(imp2, "Erode", "");
+			IJ.run(imp2, "Dilate", "");
 
 		} else {
 
