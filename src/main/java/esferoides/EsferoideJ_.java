4efbbb3f003aa9cf865ff98441b2ff1ed1cba1ed
Jonathan <joheras@gmail.com>
2019-03-29 12:37:56 +0100

    New version dealing with black holes and reorganization of code.

--- src/main/java/esferoides/EsferoideJ_.java
+++ src/main/java/esferoides/EsferoideJ_.java
@@ -40,8 +40,7 @@ public class EsferoideJ_ implements Command {
 //	@Parameter
 //	private ImagePlus imp;
 
-	
-	// Method to count the number of pixels whose value is below a threshold. 
+	// Method to count the number of pixels whose value is below a threshold.
 	private int countBelowThreshold(ImagePlus imp1, int threshold) {
 
 		ImageProcessor ip = imp1.getProcessor();
@@ -56,9 +55,11 @@ public class EsferoideJ_ implements Command {
 
 	}
 
-	// Method to draw the results stored in the roi manager into the image, and then save the
-	// image in a given directory. Since we know that there is only one esferoide per image, we
-	// only keep the ROI with the biggest area stored in the ROI Manager. 
+	// Method to draw the results stored in the roi manager into the image, and then
+	// save the
+	// image in a given directory. Since we know that there is only one esferoide
+	// per image, we
+	// only keep the ROI with the biggest area stored in the ROI Manager.
 	private static void showResultsAndSave(String dir, ImagePlus imp1, RoiManager rm) {
 		IJ.run(imp1, "RGB Color", "");
 		if (rm != null) {
@@ -78,7 +79,8 @@ public class EsferoideJ_ implements Command {
 
 	}
 
-	// Method to obtain the area from a polygon. Probably, there is a most direct method to do this. 
+	// Method to obtain the area from a polygon. Probably, there is a most direct
+	// method to do this.
 	private static final double getArea(Polygon p) {
 		if (p == null)
 			return Double.NaN;
@@ -93,9 +95,9 @@ public class EsferoideJ_ implements Command {
 		return (Math.abs(carea / 2.0));
 	}
 
-	
-	// Method to keep the ROI with the biggest area stored in the ROIManager, the rest of ROIs are
-	// deleted. 
+	// Method to keep the ROI with the biggest area stored in the ROIManager, the
+	// rest of ROIs are
+	// deleted.
 	private static void keepBiggestROI(RoiManager rm) {
 
 		Roi[] rois = rm.getRoisAsArray();
@@ -120,74 +122,80 @@ public class EsferoideJ_ implements Command {
 
 	}
 
+	private void processBlackHoles(ImagePlus imp2) {
+		IJ.setThreshold(imp2, 0, 2300);
+		IJ.run(imp2, "Convert to Mask", "");
+		IJ.run(imp2, "Fill Holes", "");
+		IJ.run(imp2, "Dilate", "");
+		IJ.run(imp2, "Watershed", "");
+	}
+
+	private void processEsferoidesGeneralCase(ImagePlus imp2) {
+		IJ.run(imp2, "Convolve...",
+				"text1=[-1 -1 -1 -1 -1 -1 -1\n-1 -1 -1 -1 -1 -1 -1\n-1 -1 -1 -1 -1 -1 -1\n-1 -1 -1 50 -1 -1 -1\n-1 -1 -1 -1 -1 -1 -1\n-1 -1 -1 -1 -1 -1 -1\n-1 -1 -1 -1 -1 -1 -1\n] normalize");
+		IJ.run(imp2, "Maximum...", "radius=2");
+		Prefs.blackBackground = false;
+		IJ.run(imp2, "Convert to Mask", "");
+		IJ.run(imp2, "Dilate", "");
+		IJ.run(imp2, "Dilate", "");
+		IJ.run(imp2, "Dilate", "");
+		IJ.run(imp2, "Fill Holes", "");
+	}
 	
-	
-	// Method to detect esferoides. 
-	private void detectEsferoide(ImporterOptions options, String dir, String name) throws FormatException, IOException {
-		options.setId(name);
-		
-		ImagePlus[] imps = BF.openImagePlus(options);
-		ImagePlus imp = imps[0];
-		ImagePlus imp2 = imp.duplicate();
+	private void processEsferoidUsingThreshold(ImagePlus imp2) {
+		IJ.setThreshold(imp2, 0, 4500);
+		IJ.run(imp2, "Convert to Mask", "");
+		IJ.run(imp2, "Dilate", "");
+		IJ.run(imp2, "Fill Holes", "");
+	}
 
-		/// We consider two cases, when there is a "black hole" in the image (the first
-		/// case), there is a lot of pixels below a given threshold, and those pixels belong to the
-		// Esferoide. 
-		int count = countBelowThreshold(imp2, 1800);
-		if (count > 10000) {
-
-			IJ.setThreshold(imp2, 0, 2300);
-			IJ.run(imp2, "Convert to Mask", "");
-			IJ.run(imp2, "Dilate", "");
-
-		} else {
-
-			IJ.run(imp2, "Convolve...",
-					"text1=[-1 -1 -1 -1 -1 -1 -1\n-1 -1 -1 -1 -1 -1 -1\n-1 -1 -1 -1 -1 -1 -1\n-1 -1 -1 50 -1 -1 -1\n-1 -1 -1 -1 -1 -1 -1\n-1 -1 -1 -1 -1 -1 -1\n-1 -1 -1 -1 -1 -1 -1\n] normalize");
-			IJ.run(imp2, "Maximum...", "radius=2");
-			Prefs.blackBackground = false;
-			IJ.run(imp2, "Convert to Mask", "");
-			IJ.run(imp2, "Dilate", "");
-			IJ.run(imp2, "Dilate", "");
-			IJ.run(imp2, "Dilate", "");
-			IJ.run(imp2, "Fill Holes", "");
-		}
-
+	private RoiManager analyzeParticles(ImagePlus imp2) {
 		IJ.run(imp2, "Analyze Particles...", "size=10000-Infinity circularity=0.10-1.00 show=Outlines exclude add");
 		ImagePlus imp3 = IJ.getImage();
 		imp2.close();
 		imp3.close();
 
 		RoiManager rm = RoiManager.getInstance();
-		
-		// We have to check whether the program has detected something (that is, whether
-		// the RoiManager is not null). If the ROIManager is empty, we try a different approach using
-		// a threshold. 
+		return rm;
+	}
+	
+	// Method to detect esferoides.
+	private void detectEsferoide(ImporterOptions options, String dir, String name) throws FormatException, IOException {
+		options.setId(name);
 
-		if (rm != null) {
-			showResultsAndSave(dir, imp, rm);
+		ImagePlus[] imps = BF.openImagePlus(options);
+		ImagePlus imp = imps[0];
+		ImagePlus imp2 = imp.duplicate();
+
+		/// We consider two cases, when there is a "black hole" in the image (the first
+		/// case), there is a lot of pixels below a given threshold, and those pixels
+		/// belong to the Esferoide.
+		int count = countBelowThreshold(imp2, 1800);
+		if (count > 10000) {
+			processBlackHoles(imp2);
 		} else {
+			processEsferoidesGeneralCase(imp2);
+		}
+		
+		RoiManager rm = analyzeParticles(imp2);
+		// We have to check whether the program has detected something (that is, whether
+		// the RoiManager is not null). If the ROIManager is empty, we try a different
+		// approach using a threshold.
+		if (rm == null) {
 			// We try to find the esferoide using a threshold directly.
 			imp2 = imp.duplicate();
-			IJ.setThreshold(imp2, 0, 4500);
-			IJ.run(imp2, "Convert to Mask", "");
-			IJ.run(imp2, "Dilate", "");
-			IJ.run(imp2, "Fill Holes", "");
-			IJ.run(imp2, "Analyze Particles...", "size=10000-Infinity circularity=0.10-1.00 show=Outlines exclude add");
-			imp3 = IJ.getImage();
-			imp2.close();
-			imp3.close();
-
-			rm = RoiManager.getInstance();
-			
-			showResultsAndSave(dir, imp, rm);
+			processEsferoidUsingThreshold(imp2);
+			rm = analyzeParticles(imp2);
 		}
+		
+		showResultsAndSave(dir, imp, rm);
 		imp.close();
 
 	}
 
-	// Method to search the list of files that satisfies a pattern in a folder. The list of files
-	// is stored in the result list. 
+	// Method to search the list of files that satisfies a pattern in a folder. The
+	// list of files
+	// is stored in the result list.
 	private static void search(final String pattern, final File folder, List<String> result) {
 		for (final File f : folder.listFiles()) {
 
@@ -208,28 +216,29 @@ public class EsferoideJ_ implements Command {
 	public void run() {
 		IJ.setForegroundColor(255, 0, 0);
 		try {
-			
+
 			// Since we are working with nd2 images that are imported with the Bio-formats
 			// plugins, we must set to true the option windowless to avoid that the program
-			// shows us a confirmation dialog every time. 
+			// shows us a confirmation dialog every time.
 			ImporterOptions options = new ImporterOptions();
 			options.setWindowless(true);
-			
-			// We ask the user for a directory with nd2 images. 
+
+			// We ask the user for a directory with nd2 images.
 			DirectoryChooser dc = new DirectoryChooser("Select the folder containing the nd2 images");
 			String dir = dc.getDirectory();
 
-			// We store the list of nd2 files in the result list. 
+			// We store the list of nd2 files in the result list.
 			File folder = new File(dir);
 			List<String> result = new ArrayList<String>();
 			search(".*\\.nd2", folder, result);
 
-			// For each nd2 file, we detect the esferoide. Currently, this means that it creates
+			// For each nd2 file, we detect the esferoide. Currently, this means that it
+			// creates
 			// a new image with the detected region marked in red.
 			for (String name : result) {
 				detectEsferoide(options, dir, name);
 			}
-			// When the process is finished, we show a message to inform the user. 
+			// When the process is finished, we show a message to inform the user.
 			IJ.showMessage("Process finished");
 
 		} catch (IOException e) {
